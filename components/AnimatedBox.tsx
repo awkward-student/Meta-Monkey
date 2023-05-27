252a1383d9c1c3839a49e036897c45bbc02d399f
Vishwas <im.vishwas.03@gmail.com>
2023-05-27 22:56:55 +0530

    Imported helpers to the scene

--- components/AnimatedBox.tsx
+++ components/AnimatedBox.tsx
@@ -1,8 +1,18 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useHelper } from "@react-three/drei";
+import { BoxHelper } from "three";
 
-const AnimatedBox = () => {
+type Props = {
+  isTesting: boolean;
+};
+
+const AnimatedBox: React.FC<Props> = ({ isTesting }) => {
   const meshRef = useRef<THREE.Mesh>(null);
+
+  {
+    isTesting ? useHelper(meshRef, BoxHelper, "Blue") : null;
+  }
 
   useFrame(() => {
     console.log("Hi");
